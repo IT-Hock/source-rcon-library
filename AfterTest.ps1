5247338e151406ea298d79ea19869fe11346f4c8
Subtixx <subtixx@gmail.com>
2018-01-24 08:56:38 +0100

    Fixed nuget package not updating version

--- AfterTest.ps1
+++ AfterTest.ps1
@@ -9,4 +9,4 @@ $nuspec = Get-ChildItem $src_dir\*.nuspec -Recurse
 Write-Host "Nuspec: $nuspec"
 Remove-Item -force -Recurse $output_dir -ErrorAction SilentlyContinue > $null
 New-Item -ItemType directory -Path $output_dir > $null
-nuget pack $nuspec.FullName -Properties "Version=$Version" -OutputDirectory $output_dir
+nuget pack $nuspec.FullName -Version $Version -OutputDirectory $output_dir
